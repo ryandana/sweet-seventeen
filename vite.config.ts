504de2f1a6a4a1c653279dd512fb9e5acce40a68
ryandana <puturyandana@gmail.com>
2025-07-17 14:01:46 +0800

    last-se

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sweet-seventeen/',
+  base: '/sweet-seventeen',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
